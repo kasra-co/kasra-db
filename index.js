a027eb1cb63c3c2dbcdea0ecb256acb324928692
Tamas Geschitz <getamas@gmail.com>
2015-04-03 18:19:11 +0200

    KSR-391

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+	init: function(options,appConfig,callback) {
+
+		var connection = null;
+		var allowedDBs = ['mongo','pg'];
+
+		if(allowedDBs.indexOf(options.db.type) === -1) {
+			return callback(['Invalid db: '+options.db.type]);
+		}
+
+		connection = require( './lib/'+options.db.type+'-connect' )(options.db.testdb,appConfig)
+		.connect(function(err,dbconn) {
+
+			if(err) {
+				return callback(['Could not connect to %s %s',options.db.type,err]);
+			}
+
+			callback(null,dbconn);
+
+		});
+	}
+};
+
+
+
+
+
+
+
