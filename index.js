ffbf7df5c03a74e06eb20d59a6f19bc70647c44a
Tamas Geschitz <getamas@gmail.com>
2015-08-16 15:02:25 +0200

    Added redis support, db connections are now reported, added hiredis driver

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 'use strict';
 
+var url = require('url');
+
 module.exports = {
 	init: function(options,appConfig,callback) {
 
 		var connection = null;
-		var allowedDBs = ['mongo','pg','memcached'];
+		var allowedDBs = ['mongo','pg','memcached','redis'];
 
 		if(allowedDBs.indexOf(options.db.type) === -1) {
 			return callback(['Invalid db: '+options.db.type]);
 		}
 
 		connection = require( './lib/'+options.db.type+'-connect' )(options.db.testdb,appConfig)
-		.connect(function(err,dbconn) {
+		.connect(function(err,dbconn,dbinfo) {
 
 			if(err) {
 				return callback(['Could not connect to %s %s',options.db.type,err]);
 			}
 
-			callback(null,dbconn);
+			callback(null,dbconn,url.format(dbinfo));
 
 		});
 	}
